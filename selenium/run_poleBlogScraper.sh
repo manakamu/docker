a6f04c2546e3ab8a18f4d22428b65f918c4df653
Manabu Nakamura <manakamumanakamu@gmail.com>
2021-08-14 12:45:48 +0900

    シェルスクリプトが動作しないのを修正

--- selenium/run_poleBlogScraper.sh
+++ selenium/run_poleBlogScraper.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-SCRIPT_DIR=$(cd $(dirname $0);pwd) #スクリプトのフルパス
-LOCK_FILE="$SCRIPT_DIR/lock.pid"
+python3 /usr/src/poleScraper/main.py >> /usr/src/poleScraper/pole.log 2>>/usr/src/poleScraper/pole-err.log
 
-if [ -e $LOCK_FILE ]; then
-  #ロックファイルが存在する場合は何もしない
-  echo "実行中。2重起動を禁止しています。"
-else
-  #ロックファイルがあれば作って処理を起動
-  echo $$ > $LOCK_FILE #とりあえずプロセスIDを書き込んでおく
-  
-  #強制終了された時には子プロセスを殺してロックファイルを削除する
-  trap 'kill $(jobs -p);rm -f $LOCK_FILE;echo 強制終了されました。' EXIT
-
-  #処理本体
-  source /usr/src/python_venv/bin/activate
-  /usr/bin/python3 $SCRIPT_DIR/main.py >>$SCRIPT_DIR/pole.log 2>>$SCRIPT_DIR/pole-err.log
-
-  #ロックファイルを削除
-  rm -f $LOCK_FILE 
-fi
