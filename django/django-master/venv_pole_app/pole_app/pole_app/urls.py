69d6044fced1fe68888b37d97f804ce56758f2ce
Manabu Nakamura <manakamumanakamu@gmail.com>
2021-10-22 22:03:28 +0900

    全削除

--- django/django-master/venv_pole_app/pole_app/pole_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pole.urls')),
-    path('accounts/', include('allauth.urls')),
-]
