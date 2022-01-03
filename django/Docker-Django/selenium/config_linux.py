1d97217b2b189f8b469f7d2a1a3f98ae736c25b0
Manabu Nakamura <manakamumanakamu@gmail.com>
2022-01-03 22:20:14 +0900

    フォルダを変更

--- django/Docker-Django/selenium/config_linux.py
+++ django/Docker-Django/selenium/config_linux.py
@@ -2,7 +2,7 @@
 CHROME_DRIVER_PATH = u'/usr/src/chromedriver'
 
 ## DBファイルのパス
-SQLITE3_DB_FILE = u'/usr/src/poleScraper/poleBlog.db'
+SQLITE3_DB_FILE = u'/usr/src/poleScraper/db/poleBlog.db'
 
 ## 画像ファイルの保存先
 IMAGE_PATH = u'/usr/src/poleScraper/images'
