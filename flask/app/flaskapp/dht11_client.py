5d47b33e95be6d1189256a67a2faf49519f10c58
manabu <manakamu2012@gmail.com>
2022-02-12 15:25:14 +0900

    新規追加

--- flask/app/flaskapp/dht11_client.py
+++ flask/app/flaskapp/dht11_client.py
@@ -1,27 +1,60 @@
+#!/usr/bin/python
+
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import Adafruit_DHT
 import requests
-import time
-import board
-import adafruit_dht
 
 URL = 'http://indigoblue.ddns.net:5000/api/sensors'
 PARAMS_FORMAT = '?sensor=DHT11&place={}&temperature={}&humidity={}'
-PLACE = 'BedRoom'
+PLACE = 'DiningRoom'
 
-dhtDevice = adafruit_dht.DHT11(board.D4)
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT11
 
-try:
-    temperature = dhtDevice.temperature
-    humidity = dhtDevice.humidity
-except:
-    # 失敗した場合は、30秒待ってからもう１回実行する
-    time.sleep(30)
-    temperature = dhtDevice.temperature
-    humidity = dhtDevice.humidity
+# Example using a Beaglebone Black with DHT sensor
+# connected to pin P8_11.
+#pin = 'P8_11'
 
-dhtDevice.exit()
+# Example using a Raspberry Pi with DHT sensor
+# connected to GPIO23.
+pin = 4
 
-params = PARAMS_FORMAT.format(PLACE, temperature, humidity)
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-response = requests.post(URL + params)
-print(response.status_code)    # HTTPのステータスコード取得
-print(response.text)    # レスポンスのHTMLを文字列で取得
+# Note that sometimes you won't get a reading and
+# the results will be null (because Linux can't
+# guarantee the timing of calls to read the sensor).
+# If this happens try again!
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    
+    params = PARAMS_FORMAT.format(PLACE, temperature, humidity)
+
+    response = requests.post(URL + params)
+    print(response.status_code)    # HTTPのステータスコード取得
+    print(response.text)    # レスポンスのHTMLを文字列で取得
+
+else:
+    print('Failed to get reading. Try again!')
